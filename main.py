0c10c71b7d18010658b60b321cef5958c71fdb43
DoiRyoto <102282406+DoiRyoto@users.noreply.github.com>
2023-07-20 11:08:40 +0900

    add: 論文のテーブルを表示できるようにした

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello 情報工学先生!!'
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0",port=81)
+    # app.run(debug=False, host="0.0.0.0",port=81)
+    app.run(debug=False, host='localhost')
