8b1e58c7c259a0aa4da702c7ab4410ee6885af8a
xi-yangchun <ittetsu1945@gmail.com>
2023-07-20 15:18:48 +0900

    [add]論文抽出とエラー捌き get_metainfo_from_titleの仕様・interfaceを変更。 num_get個の論文を取得してソート->上位num_extract個を返す. num_get>100などでダミーリストを返す。

--- call_meta_paper.py
+++ call_meta_paper.py
@@ -5,23 +5,46 @@ class PaperCaller:
     def __init__(self):
         pass
 
-    def get_metainfo_from_title(self,name_of_paper,num_paper)->dict:
+    def empty_rdata(self):
+        return [{"title":"error","abstract":"may_be_error","citationCount":-1}]
+
+    def get_metainfo_from_title(self,name_of_paper,num_get,num_extract)->dict:
+        def check_num_get_ext(num_get,num_extract):
+            if num_get>100:
+                return False
+            elif num_extract>num_get:
+                return False
+            else:
+                return True
+        
+        def check_too_many_requests(r_dict):
+            if "message" in r_dict.keys():
+                return False
+            else:
+                return True
+        
+        if check_num_get_ext(num_get,num_extract)==False:
+            return self.empty_rdata()
+
         endpoint = 'https://api.semanticscholar.org/graph/v1/paper/search'
         keyword = name_of_paper
         fields = ('title', 'year', 'referenceCount', 'citationCount',
-            'influentialCitationCount', 'isOpenAccess', 'fieldsOfStudy', 'authors')
+            'influentialCitationCount', 'isOpenAccess', 'fieldsOfStudy', 'authors',"abstract")
         params = {
             'query': keyword,
             'fields': ','.join(fields),
-            'limit': num_paper
+            'limit': num_get
         }
         r = requests.get(url=endpoint, params=params)
         
         r_dict = json.loads(r.text)
-        total = r_dict['total']        
+        if check_too_many_requests(r_dict)==False:
+            return self.empty_rdata()
+        
+        total = r_dict['total']   
         data = r_dict['data']
         data=self.sort_metainfo(data)
-        return data
+        return data[0:num_extract]
     
     def sort_metainfo(self,list_dict):
         list_dict
@@ -59,12 +82,10 @@ class PaperCaller:
                     print(f'{fi}: {d[fi]}')
     """
 
-
 """
 #usage
 pc=PaperCaller()
-data=pc.get_metainfo_from_title('python',8)
-
+data=pc.get_metainfo_from_title('python',70,10)
 for dt in data:
     print(dt['citationCount'])
 """
