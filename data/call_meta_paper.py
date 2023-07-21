40c3144bfa03ffad8e9e276c2bf523a731726d56
takumi-333 <takumi_soccer0514@icloud.com>
2023-07-21 14:54:11 +0900

    [add] 重要度（仮）でソートする機能を追加
    cとyを使った重要度を算出し、それに基づいてソートする機能

--- data/call_meta_paper.py
+++ data/call_meta_paper.py
@@ -39,18 +39,68 @@ class PaperCaller:
         if len(data)<num_extract:#
             num_extract=len(data)
         self.extract_names(data)
-        data=self.sort_metainfo(data)
+        self.culcurate_importance(data,0.5)
+        data=self.sort_metainfo_by_importance(data)
         for dt in data:
             dt.pop("paperId")
         return data[0:num_extract]
     
     def sort_metainfo(self,list_dict):
-        #list_dict
         list_dict_sorted = sorted(list_dict,
                                    key=lambda x:x['citationCount'],reverse=True)
         return list_dict_sorted
+    
+    def sort_metainfo_by_importance(self, list_dict):
+        list_dict_sorted = sorted(list_dict,
+                                   key=lambda x:x['importance'],reverse=True)
+        return list_dict_sorted
 
     """
+    i = alpha * c + (1 - alpha) * yで計算
+    Noneのデータは平均を用いて計算
+    """
+    def culcurate_importance(self, list_dict, alpha):
+        citationCount = []
+        year = []
+        for dt in list_dict:
+            if (dt['citationCount'] != None):
+                citationCount.append(int(dt['citationCount']))
+            if (dt['year'] != None):
+                year.append(int(dt['year']))
+
+        if (len(citationCount) != 0):
+            citationCount_max = max(citationCount)
+            citationCount_min = min(citationCount)
+            citationCount_avg = sum(citationCount) / len(citationCount)
+        else:
+            citationCount_max = 0
+            citationCount_min = 0
+            citationCount_avg = 0
+            alpha = 0
+        if (len(year) != 0):
+            year_max = max(year)
+            year_min = min(year)
+            year_avg = sum(year) / len(year)
+        else:
+            year_max = 0
+            year_min = 0
+            year_avg = 0
+            alpha = 1
+        
+        for dt in list_dict:
+            if (dt['citationCount'] != None):
+                c = ((int(dt['citationCount']) - citationCount_min) / (citationCount_max - citationCount_min + 0.01))  #　0除算回避
+            else:
+                c = ((citationCount_avg - citationCount_min) / (citationCount_max - citationCount_min + 0.01))
+            if (dt['year'] != None):
+                y = ((int(dt['year']) - year_min) / (year_max - year_min + 0.01))
+            else:
+                y = ((year_avg - year_min) / (year_max - year_min + 0.01))
+            i = alpha * c + (1 - alpha) * y
+            dt['importance'] = i
+
+    """
+    Convert List of Dict to String
     論文データの著者名をStringに変換
     """
     def extract_names(self, list_dict):
@@ -97,10 +147,11 @@ class PaperCaller:
 
 """
 #usage
+keyword = input("Please input keywords ")
 pc=PaperCaller()
-data=pc.get_metainfo_from_title('C',1,1)
+data=pc.get_metainfo_from_title(keyword,50,50)
 for dt in data:
-    print(dt['authors'])
-
+    print(dt['importance'])
 """
+
 
