ed3d1a07794eeb4476b99ce75a512d6335105818
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2023-07-24 21:46:41 +0900

    update: 論文タイトルをクリックして遷移できるようになった

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import sqlite3
+from flask import Flask, render_template, request, url_for, redirect, g
 from data.papers import get_papers_data
 from data.call_meta_paper import PaperCaller
 
@@ -6,22 +7,31 @@ app = Flask(__name__)
 pc = PaperCaller()
 
 
+def get_db():
+    if 'db' not in g:
+         g.db = sqlite3.connect('TestDB.db')
+    return g.db
+
 @app.route('/', methods=["GET", "POST"])
 def index():
   if request.method == 'POST':
     keyword = request.form["keyword"]
-    # APIに入力キーワードと表示する論文数を渡す
-    num_get=1000
-    main_paper, papers_data = pc.get_metainfo_from_title(keyword, num_get, num_get)
 
-    if len(papers_data) != 0:
-      keys = papers_data[0].keys()
-      return render_template('index.html',
-                             n=len(keys),
-                             papers=papers_data,
-                             keys=keys)
+    return redirect(url_for("papers" , keyword = keyword))
 
   return render_template('index.html')
+
+@app.route("/papers/<string:keyword>", methods=["GET", "POST"])
+def papers(keyword):
+  num_get=1000
+  _, papers_data = pc.get_metainfo_from_title(keyword, num_get, num_get)
+
+  if len(papers_data) != 0:
+    keys = papers_data[0].keys()
+    return render_template("papers.html", n=len(keys), papers=papers_data, keys=keys)
+  
+  return render_template("papers.html")
+
 
 """
 Localでhtmlを表示する場合はhost="localhost"
