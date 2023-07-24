eb162315de6373d24df837eb2a1e8467d1a23268
xi-yangchun <ittetsu1945@gmail.com>
2023-07-24 11:18:12 +0900

    [update]例外処理の追加 例外処理は空文字列を返して対応する。 - 抽出する単語数が少ない時(<num_extract)、足りないぶん空文字列を返す。 - Noneを入力すると空文字列のリストを返す。

--- extract_by_rake.py
+++ extract_by_rake.py
@@ -7,20 +7,27 @@ class Rake_Keyword_Extractor():
     def get_keywords(self,doc:str,num_extract=5)->list:
         # Uses stopwords for english from NLTK, and all puntuation characters by
         # default
+        if doc==None:
+            return ['' for i in range(num_extract)]
         r = Rake()
         #Extraction given the text.
         r.extract_keywords_from_text(doc)
         
         # To get keyword phrases ranked highest to lowest with scores.        
         ret=[]
-        top_results=r.get_ranked_phrases_with_scores()[0:num_extract]
-        for result in top_results:
-            ret.append(result[1])
-        print(ret)
+        #top_results=r.get_ranked_phrases_with_scores()[0:num_extract]
+        result=r.get_ranked_phrases_with_scores()
+        for i in range(num_extract):
+            if i<=len(result)-1:
+                ret.append(result[i][1])
+            else:
+                ret.append("")
         return ret
+
 
 """
 #usage
 RE=Rake_Keyword_Extractor()
-RE.get_keywords("input your favorite doc",7)
+res=RE.get_keywords("input your favorite doc",7)
+print(res)
 """
