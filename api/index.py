a4600dc0e0786bdb57e0b03ab4fe00349dea9018
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2023-08-02 17:08:34 +0900

    update: リファクタリング

--- api/index.py
+++ api/index.py
@@ -1,15 +1,9 @@
-import sqlite3
-from flask import Flask, render_template, request, url_for, redirect, g
+from flask import Flask, render_template, request, url_for, redirect
 from data.call_meta_paper import PaperCaller
 
 app = Flask(__name__)
 pc = PaperCaller()
 
-
-def get_db():
-    if 'db' not in g:
-         g.db = sqlite3.connect('TestDB.db')
-    return g.db
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -33,7 +27,7 @@ def root(keyword):
       return redirect(url_for("papers" , paperId = paperId))
 
   num_get=1000
-  papers_data = pc.get_metainfo_from_keyword(keyword, num_get, num_get)
+  papers_data = pc.get_papers_by_keyword(keyword, num_get, num_get)
   
   if len(papers_data) != 0:
     keys = papers_data[0].keys()
@@ -49,12 +43,10 @@ def papers(paperId):
 
     return redirect(url_for("root" , keyword = keyword))
   
-  num_get=1000
   paperIds=paperId.split('-')
-  for id in paperIds:
-    print(id)
-  targ_paper=paperIds[-1]
-  main_data, papers_data = pc.get_metainfo_from_paperId(targ_paper, num_get, num_get)
+  main_paper_id=paperIds[-1]
+  main_data = pc.get_paper_by_paperId(main_paper_id)
+  papers_data = pc.get_reference_papers_by_main_paper_id(main_paper_id)
 
   if len(papers_data) != 0:
     keys = papers_data[0].keys()
@@ -79,7 +71,7 @@ def result(paperId):
   paperIds=paperId.split('-')
   # for id in paperIds:
   #   print(id)
-  papers_data = pc.get_metainfo_from_paperIds(paperIds)
+  papers_data = pc.get_papers_by_paperIds(paperIds)
 
   # return render_template("result_nontable.html", papers=papers_data)
   return render_template("result.html", papers=papers_data)
